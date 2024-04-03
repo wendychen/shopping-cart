09f90562cf130cd7cddf3be12088429a94551bf5
Wendy Chen <lfwendy@gmail.com>
2024-04-03 17:39:19 +0800

    version 1

---
+++ src/pages/home.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { Circles } from "react-loader-spinner";
+import ProductTile from "../components/product-tile";
+
+export default function Home() {
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    async function fetchListOfProducts() {
+        setLoading(true);
+        const res = await fetch('https://fakestoreapi.com/products');
+        const data = await res.json();
+
+        if(data) {
+            setLoading(false);
+            setProducts(data);
+        }
+    }
+
+    useEffect(() => {
+        fetchListOfProducts();
+    }, []);
+
+    return <div>
+        {
+            loading ? (
+                <div className="min-h-screen w-full flex justify-center items-center">
+                    <Circles
+                        height={'60'}
+                        width={'60'}
+                        color="rgb(59, 29, 69)"
+                        visible={true}
+                    />
+                </div> 
+            ) : 
+            <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+                {
+                    products && products.length ?
+                    products.map(productItem => <ProductTile product={productItem} />)
+                    : null
+                }
+            </div>
+        }
+    </div>;
+}
